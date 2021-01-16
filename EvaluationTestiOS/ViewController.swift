15ffb3f3d67cba5f62c3c753ece48171d2ae6ebd
Денис Магильницкий <magilnitsky@gmail.com>
2021-01-16 11:01:58 +0300

    first commit

---
+++ EvaluationTestiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EvaluationTestiOS
+//
+//  Created by Денис Магильницкий on 16.01.2021.
+//  Copyright © 2021 Денис Магильницкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
